32bd44886c349d576f19f61fd18c1b0b8c3bc3b8
蔣永寬 <mrzackchiang@gmail.com>
2026-01-20 15:38:36 +0900

    first commit of collision game

--- collisionGame/script.js
+++ collisionGame/script.js
@@ -6,6 +6,9 @@ const collisionCtx = collisionCanvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width = 1000;
 const CANVAS_HEIGHT = canvas.height = 600;
 
+collisionCanvas.width = CANVAS_WIDTH;
+collisionCanvas.height = CANVAS_HEIGHT;
+
 const enemyImage = new Image();
 enemyImage.src = "fire-skull.png";
 
@@ -17,7 +20,8 @@ class Enemy {
      constructor() {
           this.spriteHeight = 112;
           this.spriteWidth = 96;
-          this.x = CANVAS_WIDTH/2 + Math.random() * CANVAS_WIDTH/2 - CANVAS_WIDTH/4;
+          // this.x = CANVAS_WIDTH/2 + Math.random() * CANVAS_WIDTH/2 - CANVAS_WIDTH/4;
+          this.x = CANVAS_WIDTH + Math.random() * 5;
                   // Define movement boundaries
           this.minY = 100;  // Top boundary
           this.maxY = CANVAS_HEIGHT - this.spriteHeight - 100;  // Bottom boundary
@@ -28,10 +32,12 @@ class Enemy {
           
           this.spriteFrame = 0;
           this.image = enemyImage;
-          this.speedModifier = Math.random() + 1 ;
+          this.speedModifier = Math.random() * 5 ;
           this.speed = Math.floor(gameSpeed * this.speedModifier);
           this.angle = Math.random() * 10;
           this.frameSpeed = Math.floor(Math.random() * 10 + 10);
+
+          this.color = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
 
           this.shouldRemove = false;
      }
@@ -39,7 +45,7 @@ class Enemy {
      update() {
           this.x = this.x - this.speed;
 
-          if (this.x + this.spriteWidth < 0) this.x = CANVAS_WIDTH;
+          if (this.x + this.spriteWidth < 0) this.shouldRemove = true;
           
           // Calculate vertical movement
           let newY = this.y + Math.sin(this.angle) * 3;
@@ -62,7 +68,12 @@ class Enemy {
      }
 
      draw() {
+          collisionCtx.fillStyle = this.color;
+
+          collisionCtx.strokeStyle = "red";      // border color
+          collisionCtx.lineWidth = 3;            // border thickness
           collisionCtx.fillRect(this.x, this.y, this.spriteWidth, this.spriteHeight);
+          
           ctx.drawImage(this.image, this.spriteFrame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, 
                this.x, this.y, this.spriteWidth, this.spriteHeight);
      }
@@ -73,7 +84,7 @@ let enemyNumControl = document.getElementById("enemyNum");
 
 let enemies = [];
 
-initEnemies(enemyNumControl.value);
+// initEnemies(enemyNumControl.value);
 
 enemyNumControl.addEventListener("change", (e) => {
      enemyNum = e.target.value;
@@ -89,17 +100,25 @@ function initEnemies(enemyNum) {
      }
 }
 
+let lastTime = 0;
+let deltaTime;
+let intervalTime = 0;
+const enemyInterval = 1000;
 
+function animate(timeStamp) {
+     deltaTime = timeStamp - lastTime;
+     intervalTime += deltaTime;
 
+     if (intervalTime > enemyInterval){
+          enemies.push(new Enemy());
+          intervalTime = 0;
+     }
+     lastTime = timeStamp;
 
-function animate() {
      ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
      collisionCtx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-     
-     // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-     // ctx.drawImage(enemyImage, spriteFrame * spriteWidth, 0, spriteWidth, spriteHeight, 
-     //      frameX, frameY, spriteWidth, spriteHeight);
 
+     enemies = enemies.filter(enemy => !enemy.shouldRemove);
      enemies.forEach(enemy => {
           enemy.update();
           enemy.draw();
@@ -110,4 +129,4 @@ function animate() {
      requestAnimationFrame(animate);
 }
 
-animate();
+animate(0);
